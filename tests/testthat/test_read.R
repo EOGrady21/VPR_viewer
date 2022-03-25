11a469d7a26060e1322ce74750be4ea3e7b4d57c
EOGrady21 <38440373+EOGrady21@users.noreply.github.com>
2022-03-25 12:53:29 -0300

    test CTD data

--- tests/testthat/test_read.R
+++ tests/testthat/test_read.R
@@ -1,17 +1,20 @@
-context("tests work")
+context("Read in CTD data...")
 
-# test_file <- 'www/IML2018051/rois/vpr0/d286/h22ctd.dat'
-# ctd_col <- c("time_ms", "conductivity", "temperature", "pressure", "salinity", "fluor_ref", "fluorescence_mv",
-#              "turbidity_ref", "turbidity_mv", "altitude_NA")
-# test_that("CTD file is read in", {
-#   expect_silent(ctd_dat <- vpr_ctd_read(test_file, 
-#                           station_of_interest = NA, 
-#                           day = '286', 
-#                           hour = '22',
-#                           col_list = ctd_col)
-#   )
-# })
-
-test_that("tests on GH are working",{
-  expect_equal(1, 1)
+test_file <- system.fle('www/IML2018051/rois/vpr0/d286',
+                        'h22ctd.dat',
+                        package = 'VPR_viewer',
+                        mustWork = TRUE)
+ctd_col <- c("time_ms", "conductivity", "temperature", "pressure", "salinity", "fluor_ref", "fluorescence_mv",
+             "turbidity_ref", "turbidity_mv", "altitude_NA")
+test_that("CTD file is read in", {
+  expect_silent(ctd_dat <- vpr_ctd_read(test_file,
+                          station_of_interest = NA,
+                          day = '286',
+                          hour = '22',
+                          col_list = ctd_col)
+  )
 })
+
+# test_that("tests on GH are working",{
+#   expect_equal(1, 1)
+# })
